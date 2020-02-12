a9ca06bcbb55f46e9a8e4bcd9b42df0e75d480e8
NightOwl <steelman2019e@gmail.com>
2020-02-12 18:13:06 +0900

    + Update 보안성
    Token 정보 노출 문제 Fix.
    
    commit log에 토큰 정보가 남는데 처리하기 귀찮으니 방치.

--- rest-server/src/main/java/com/nigjhtowl/rest_server/FCMController.java
+++ rest-server/src/main/java/com/nigjhtowl/rest_server/FCMController.java
@@ -4,11 +4,11 @@ import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import util.FCMUtil;
+import util.myConstant.FCM_Constant;
 
 @Controller
 public class FCMController {
@@ -21,10 +21,10 @@ public class FCMController {
 	public String fcmtest(String title, String content) throws Exception {
 
 		// console.firebase.google.com에서 받은 JSON파일
-		String tokenPath = application.getRealPath("resources/google/fcm-test-b4e16-firebase-adminsdk-etdt4-16f9949f01.json");
+		String tokenPath = application.getRealPath(FCM_Constant.TOKEN_PATH);
 
 		// Android폰에서 받는 토큰값
-		String tokenId = "cIHMCj4PfDM:APA91bGhfwbtR71qxogm7FuNMp0Hi5q7XKlNBZJ2EKx7qn6ZfX_nSg3sb9LtyWq4S3CaRet5QGurSRH_aLPOOaZZc330KvJHZep6wm0uFFGnohPZAiXJWCciI4ebp-M_B5o4sUvx558y";
+		String tokenId = FCM_Constant.TOKEN_ID;
 
 		new FCMUtil().send_FCM(tokenPath, tokenId, title, content);
 
